80825e0e57e1633fa16750e6fe99e7af67ea3859
MycroftBrooks <akim.zemar@gmail.com>
2022-11-22 16:39:19 +0300

    added possibility to parse trough search bar

--- parser/parser_app/views.py
+++ parser/parser_app/views.py
@@ -6,21 +6,27 @@ from decouple import config
 
 title_list=[]
 
-def reddit_parser():
+def reddit_parser(word_input):
     reddit = praw.Reddit(client_id= config("client_id", default=''), client_secret= config("client_secret", default=''), user_agent= config("user_agent", default=''))
-    AskReddit_subreddit  = reddit.subreddit('AskReddit').search('issue', limit=10)
+    AskReddit_subreddit  = reddit.subreddit('AskReddit').search(word_input, limit=20)
     for post in AskReddit_subreddit:
         title = post.title
         data = {'title': title }
         title_list.append(data)
     print(title_list)
 
-reddit_parser()
+#reddit_parser()
 
 
 def index(request):
     context = {
         'title_list': title_list
     }
-    return render(request, 'parser_app/index.html', context)
+    if request.method == "POST":
+        title_list.clear()
+        word_input = str(request.POST["word_input"])
+        reddit_parser(word_input)
+        return render(request, 'parser_app/index.html', context)
+    else:
+        return render(request, "parser_app/index.html")
 
