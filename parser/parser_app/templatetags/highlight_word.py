c73340c2be015440667305b2f3e192b1a9d46635
Tut1tam <dolzhich.maxim@yandex.ru>
2022-11-26 01:21:06 +0300

    DELETE templatetag highlight_word

--- parser/parser_app/templatetags/highlight_word.py
+++ parser/parser_app/templatetags/highlight_word.py
@@ -1,11 +1,15 @@
+import html
+
 from django import template
 from django.template.defaultfilters import stringfilter
 from parser_app import views
 
 register = template.Library()
 
-@register.filter
-@stringfilter
-def highlight_word(post, word_input):
-    word_input = views.get_word_input(word_input)
-    return post.replace(word_input, "<h5><u>" + word_input + "</u></h5>")
+
+# @register.filter
+# @stringfilter
+# def highlight_word(post):
+#     word_input = views.get_word_input(word_input)
+#     s = post.replace(word_input, "<h5><u>" + word_input + "</u></h5>")
+#     return html.escape(s, quote=True)
