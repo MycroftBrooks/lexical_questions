989d6743dca91ddc085b9e0b6c002d20b03fdf28
MycroftBrooks <akim.zemar@gmail.com>
2022-12-15 16:04:14 +0300

    DEL extra comments

--- parser/parser_app/functions_python/reddit_parser.py
+++ parser/parser_app/functions_python/reddit_parser.py
@@ -10,10 +10,6 @@ from decouple import config
 from better_profanity import profanity
 
 def reddit_parser(word_input, title_list):
-    """ url = f"https://www.reddit.com/r/AskReddit/search/?q={word_input}&restrict_sr=1&sr_nsfw=&sort=hot"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"}
-    r = requests.get(url, headers=headers)
-    soup = bs4.BeautifulSoup(r.text, "html.parser") """
     reddit = praw.Reddit(
         client_id=config("client_id", default=""),
         client_secret=config("client_secret", default=""),
@@ -35,34 +31,6 @@ def reddit_parser(word_input, title_list):
         data = {"title": title}
         title_list.append(data)
     else:
-        """ try:
-            all_titles = soup.find("div", class_="QBfRw7Rj8UkxybFpX-USO").findAll("h3", class_="_eYtD2XCVieq6emjKBH3m")
-            #for el in all_titles:
-                print(el.text)
-                #data = {"title": el.text}
-                #title_list.append(data)  
-            for el in all_titles:
-                el = el.text
-                if profanity.contains_profanity(el) == 0:
-                    print('its ok')
-                    mut = words & set(re.findall(r"(\w+)", el.casefold()))
-                    if len(mut) == 0:
-                        w = word_input[0].lower()
-                        w1 = word_input[0].upper()
-                        word = f"\\b[{w}{w1}]{word_input[1:]}\\w?\\b"
-                        res = re.findall(word, el)
-                        for e in res:
-                            el = el.replace(
-                                e, f"<mark><span style='color:green'>{e}</span></mark>"
-                            )
-                        data = {"title": el}
-                        title_list.append(data)
-                    else:
-                        continue
-        except AttributeError as err:
-            title = "NO RESULTS FOUND"
-            data = {"title": title}
-            title_list.append(data) """
         AskReddit_subreddit = reddit.subreddit("AskReddit").search(
             query=word_input, sort="hot", limit=30
         )
