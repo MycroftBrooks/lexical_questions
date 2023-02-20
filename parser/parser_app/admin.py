0659b0ea50e41e2d8b104b37f918d1c4ead6309a
MycroftBrooks <akim.zemar@gmail.com>
2023-02-20 18:58:09 +0300

    ADD Bookpdf model to admin.py

--- parser/parser_app/admin.py
+++ parser/parser_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ChildQuestions, Test, Question
+from .models import ChildQuestions, Test, Question, Bookpdf
 
 # Register your models here.
 admin.site.register(ChildQuestions)
+admin.site.register(Bookpdf)
 
 
 class QuestionInlineAdmin(admin.TabularInline):
