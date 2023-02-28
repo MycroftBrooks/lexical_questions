308050b6e02778f0c04dedd30ba2be2250d93c80
MycroftBrooks <akim.zemar@gmail.com>
2023-02-28 17:50:27 +0300

    ADD Bookpdf model

--- parser/parser_app/models.py
+++ parser/parser_app/models.py
@@ -24,3 +24,10 @@ class Question(models.Model):
     test = models.ForeignKey(Test, related_name="questions", on_delete=models.CASCADE)
     question = models.CharField(max_length=200)
     right_answer = models.CharField(max_length=1, choices=ANSWER_CHOICES)
+
+
+class Bookpdf(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    pdf = models.FileField(upload_to="pdfs/")
+    students = models.ManyToManyField(User, related_name="studentsBook", blank=True)
