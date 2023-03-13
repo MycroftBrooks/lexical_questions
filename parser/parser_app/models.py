6dcba82780366188f65a743a3ef538dadb7ea342
MycroftBrooks <akim.zemar@gmail.com>
2023-03-13 20:02:53 +0300

    ADD Quiz model

--- parser/parser_app/models.py
+++ parser/parser_app/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# from django.contrib.auth.models import User
 
-# Create your models here.
+ANSWER_CHOICES = (
+    ("a", "a"),
+    ("b", "b"),
+    ("c", "c"),
+    ("d", "d"),
+)
+
+
 class ChildQuestions(models.Model):
     question = models.CharField(max_length=200)
+
+
+class Quiz(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.CharField(max_length=300)
+    answer = models.CharField(max_length=1, choices=ANSWER_CHOICES)
+
+    def __str__(self):
+        return self.question
