331b84ccb3ad1270ecfc5ee58164adeb4cdac43a
MycroftBrooks <akim.zemar@gmail.com>
2023-03-14 23:41:10 +0300

    ADD url for creation test

--- parser/parser/urls.py
+++ parser/parser/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from theme.views import change_theme
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("switch-theme/", change_theme, name="change_theme"),
